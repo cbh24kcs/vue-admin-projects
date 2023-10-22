8d198bbbbd62fc2ef1067aea6cbffb69c65d5a52
cbh24kcs <cbh@qq.com>
2023-10-23 02:08:28 +0800

    feat: axios封装调整

--- cbh-admin-vue3/src/utils/request.ts
+++ cbh-admin-vue3/src/utils/request.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
 
 const instance = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_API,
   headers: {
     "Content-Type": "application/json",
   },
+  timeout: 10000,
 });
 
+// 请求拦截器
 instance.interceptors.request.use(
   (config) => {
     return config;
@@ -15,6 +18,7 @@ instance.interceptors.request.use(
   }
 );
 
+// 响应拦截器
 instance.interceptors.response.use(
   (response) => {
     return response.data;
