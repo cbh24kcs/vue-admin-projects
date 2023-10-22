ea6e7dff08400f9e2d26d00933811fa8e50ed3bb
cbh24kcs <cbh@qq.com>
2023-10-23 02:08:44 +0800

    test: 菠萝学习测试

--- cbh-admin-vue3/src/store/index.ts
+++ cbh-admin-vue3/src/store/index.ts
@@ -2,11 +2,11 @@ import { createPinia, defineStore } from "pinia";
 
 export const pinia = createPinia();
 
-// 定义一个默认存储
-export const useDefaultStore = defineStore("default", {
-  state: () => ({
-    count: 1,
-  }),
-  getters: {},
-  actions: {},
-});
+// // 定义一个默认存储
+// export const useDefaultStore = defineStore("default", {
+//   state: () => ({
+//     count: 1,
+//   }),
+//   getters: {},
+//   actions: {},
+// });
