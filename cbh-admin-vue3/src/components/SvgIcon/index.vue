4ac10397bfb60389da74f6d2ad82d29f7f610baf
cbh24kcs <cbh@qq.com>
2023-10-23 02:11:27 +0800

    test: svg测试

---
+++ cbh-admin-vue3/src/components/SvgIcon/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+
+// defineProps({
+//   icon: {
+//     type: String,
+//     default: ''
+//   }
+// })
+
+interface Props {
+  icon: string;
+  className?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), { icon: '' })
+</script>
+
+<style scoped></style>
