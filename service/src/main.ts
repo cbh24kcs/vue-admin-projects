641f85646b6ba501a3eadf9f978ee1d547e2960d
cbh24kcs <cbh@qq.com>
2023-11-29 22:03:24 +0800

    feat: 后端暂提

--- service/src/main.ts
+++ service/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from "path";
 
+// swagger-ui-express @nestjs/swagger nestjs-knife4j
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { knife4jSetup } from "nestjs-knife4j";
+
 const host = "http://127.0.0.1";
 const port = 3000;
 
@@ -26,6 +30,24 @@ async function bootstrap() {
 
   // 设置全局接口前缀
   // app.setGlobalPrefix("api")
+
+  const options = new DocumentBuilder()
+    .setTitle("CBH ADMIN")
+    .setDescription("The cbh API description")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("api", app, document);
+  knife4jSetup(app, {
+    urls: [
+      {
+        name: "V0.1 版本",
+        url: `/api-json`,
+        swaggerVersion: "3.0",
+        location: `/api-json`,
+      },
+    ],
+  });
 
   await app.listen(port, () => {
     console.log(`服务启动成功, ${host}:${port}`);
